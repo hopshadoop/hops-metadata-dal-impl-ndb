c28d45a294d09789965afadd696e23109ed9b0ea
Jim Dowling <jdowling@sics.se>
2015-08-12 10:29:25 +0200

    script to deploy hops-metadata-dal-ndb-impl.jar to our artifact repo

---
+++ deploy-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 2 ] ; then
+  echo "usage: <prog> hadoop_version ndb_version (e.g., 2.4.0 7.4.6)"
+  exit 1
+fi
+
+server=glassfish@snurran.sics.se:/var/www/hops
+
+mvn clean install assembly:assembly -DskipTests
+scp target/hops-metadata-dal-impl-ndb-1.0-SNAPSHOT-jar-with-dependencies.jar $server/ndb-dal-$1-$2.jar
+
