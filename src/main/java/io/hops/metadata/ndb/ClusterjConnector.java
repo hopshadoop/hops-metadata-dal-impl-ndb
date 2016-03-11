02316211348db3777e89523bdeae7dd90130a6bb
Gautier Berthou <gautier.berthou@gmail.com>
2016-03-11 11:30:03 +0100

    remove unused tables

--- src/main/java/io/hops/metadata/ndb/ClusterjConnector.java
+++ src/main/java/io/hops/metadata/ndb/ClusterjConnector.java
@@ -110,9 +110,7 @@ import io.hops.metadata.yarn.dal.UpdatedContainerInfoDataAccess;
 import io.hops.metadata.yarn.dal.YarnProjectsDailyCostDataAccess;
 import io.hops.metadata.yarn.dal.YarnProjectsQuotaDataAccess;
 import io.hops.metadata.yarn.dal.YarnVariablesDataAccess;
-import io.hops.metadata.yarn.dal.capacity.CSLeafQueueUserInfoDataAccess;
 import io.hops.metadata.yarn.dal.capacity.CSLeafQueuesPendingAppsDataAccess;
-import io.hops.metadata.yarn.dal.capacity.CSQueueDataAccess;
 import io.hops.metadata.yarn.dal.capacity.FiCaSchedulerAppReservedContainersDataAccess;
 import io.hops.metadata.yarn.dal.fair.LocalityLevelDataAccess;
 import io.hops.metadata.yarn.dal.fair.RunnableAppsDataAccess;
@@ -412,13 +410,11 @@ public class ClusterjConnector implements StorageConnector<DBSession> {
         RMLoadDataAccess.class, PendingEventDataAccess.class,
         LocalityLevelDataAccess.class,
         RunnableAppsDataAccess.class,
-        CSQueueDataAccess.class,
         NextHeartbeatDataAccess.class,
         NodeHBResponseDataAccess.class, 
         YarnProjectsQuotaDataAccess.class, YarnProjectsDailyCostDataAccess.class,
         ContainersCheckPointsDataAccess.class,
-        CSLeafQueuesPendingAppsDataAccess.class,
-        CSLeafQueueUserInfoDataAccess.class);
+        CSLeafQueuesPendingAppsDataAccess.class);
   }
 
   private boolean format(boolean transactional,
@@ -679,12 +675,8 @@ public class ClusterjConnector implements StorageConnector<DBSession> {
             truncate(transactional, io.hops.metadata.yarn.TablesDef.LocalityLevelTableDef.TABLE_NAME);
           } else if (e== RunnableAppsDataAccess.class){
             truncate(transactional, io.hops.metadata.yarn.TablesDef.RunnableAppsTableDef.TABLE_NAME);
-          } else if (e==CSQueueDataAccess.class){
-            truncate(transactional, io.hops.metadata.yarn.TablesDef.CSQueueTableDef.TABLE_NAME);
           } else if (e==CSLeafQueuesPendingAppsDataAccess.class){
             truncate(transactional, io.hops.metadata.yarn.TablesDef.CSLeafQueuesPendingAppsTableDef.TABLE_NAME);
-          } else if (e==CSLeafQueueUserInfoDataAccess.class){
-            truncate(transactional, io.hops.metadata.yarn.TablesDef.CSLeafQueueUserInfoTableDef.TABLE_NAME);
           }
         }
         MysqlServerConnector.truncateTable(transactional,
