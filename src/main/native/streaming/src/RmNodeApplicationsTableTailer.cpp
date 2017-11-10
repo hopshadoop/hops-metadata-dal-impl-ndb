58c5669c85e96709c93217ba8837e912767ff9a9
Gautier Berthou <gautier.berthou@gmail.com>
2017-11-10 16:58:37 +0100

    [HOPS-128] replace table yarn_rmnode_finishedapplications by yarn_rmnode_applications in libhopsyarn

--- src/main/native/streaming/src/RmNodeApplicationsTableTailer.cpp
+++ src/main/native/streaming/src/RmNodeApplicationsTableTailer.cpp
@@ -22,44 +22,44 @@
  * 
  */
 
-#include "FinishedApplicationsTableTailer.h"
+#include "RmNodeApplicationsTableTailer.h"
 
 using namespace Utils::NdbC;
 
-const string _finishedApplications_table= "yarn_rmnode_finishedapplications";
-const int _finishedApplications_noCols= 3;
-const string _finishedApplications_cols[_finishedApplications_noCols]=
+const string _rmNodeApplications_table= "yarn_rmnode_applications";
+const int _rmNodeApplications_noCols= 3;
+const string _rmNodeApplications_cols[_rmNodeApplications_noCols]=
     {"rmnodeid",
      "applicationid",
      "status"
     };
 
-const int _finishedApplications_noEvents = 2; 
-const NdbDictionary::Event::TableEvent _finishedApplications_events[_finishedApplications_noEvents] = 
+const int _rmNodeApplications_noEvents = 2; 
+const NdbDictionary::Event::TableEvent _rmNodeApplications_events[_rmNodeApplications_noEvents] = 
     { NdbDictionary::Event::TE_INSERT, 
       NdbDictionary::Event::TE_UPDATE
     };
 
-const WatchTable FinishedApplicationsTableTailer::TABLE = {_finishedApplications_table, _finishedApplications_cols, _finishedApplications_noCols , _finishedApplications_events, _finishedApplications_noEvents};
+const WatchTable RmNodeApplicationsTableTailer::TABLE = {_rmNodeApplications_table, _rmNodeApplications_cols, _rmNodeApplications_noCols , _rmNodeApplications_events, _rmNodeApplications_noEvents};
 
 const int RMNODE_ID = 0;
 const int APPLICATION_ID = 1;
 const int STATUS = 2;
 
-FinishedApplicationsTableTailer::FinishedApplicationsTableTailer(Ndb* ndb, const int poll_maxTimeToWait,JavaVM* jvm) 
+RmNodeApplicationsTableTailer::RmNodeApplicationsTableTailer(Ndb* ndb, const int poll_maxTimeToWait,JavaVM* jvm) 
   : TableTailer(ndb, TABLE, poll_maxTimeToWait, jvm){
   
   
 }
 
-void FinishedApplicationsTableTailer::handleEvent(NdbDictionary::Event::TableEvent eventType, NdbRecAttr* preValue[], NdbRecAttr* value[]) {
+void RmNodeApplicationsTableTailer::handleEvent(NdbDictionary::Event::TableEvent eventType, NdbRecAttr* preValue[], NdbRecAttr* value[]) {
   if(!attached){
     if (jvm->AttachCurrentThread((void**)&env, NULL)!= 0) {
       std::cout << "Failed to attach" << std::endl;
     }
-    cls = env->FindClass("io/hops/streaming/FinishedApplicationsEventReceiver");
+    cls = env->FindClass("io/hops/streaming/RmNodeApplicationsEventReceiver");
     jmethodID midInit = env->GetMethodID( cls, "<init>", "()V");
-    finishedApplicationsEventReceiver = env->NewObject( cls, midInit);
+    rmNodeApplicationsEventReceiver = env->NewObject( cls, midInit);
   
     midCreateAndAddToQueue = env->GetMethodID(cls, "createAndAddToQueue","(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
     attached = true;
@@ -70,14 +70,14 @@ void FinishedApplicationsTableTailer::handleEvent(NdbDictionary::Event::TableEve
   jstring status = env->NewStringUTF(get_string(value[STATUS]).c_str());
   //create the rmnode object and put it in the event queue
     LOG_INFO("create event");
-    env->CallVoidMethod(finishedApplicationsEventReceiver,midCreateAndAddToQueue,rmnodeId, applicationId, status);
+    env->CallVoidMethod(rmNodeApplicationsEventReceiver,midCreateAndAddToQueue,rmnodeId, applicationId, status);
   env->DeleteLocalRef(rmnodeId);
   env->DeleteLocalRef(applicationId);
   env->DeleteLocalRef(status);
    
 }
 
-FinishedApplicationsTableTailer::~FinishedApplicationsTableTailer() {
+RmNodeApplicationsTableTailer::~RmNodeApplicationsTableTailer() {
 
 }
 
