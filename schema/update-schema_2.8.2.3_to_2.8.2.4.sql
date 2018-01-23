3a84dc2dcaadcf98e96352eaba1dbf9825eb45fd
August Bonds <august.bonds@protonmail.com>
2018-01-23 15:28:50 +0100

    [HOPS-5] Hops ACLs.
    avoiding redundant obtainSession calls
    
    adding acl tests and fixing subtree permission checks for rename

--- schema/update-schema_2.8.2.3_to_2.8.2.4.sql
+++ schema/update-schema_2.8.2.3_to_2.8.2.4.sql
@@ -20,9 +20,23 @@ CREATE TABLE `hdfs_retry_cache_entry` (
   PRIMARY KEY (`client_id`, `call_id`),
   INDEX `expiration_time` (`expiration_time` ASC)
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1;
- 
+
 ALTER TABLE `hdfs_pending_blocks`
 ADD COLUMN `target` varchar(255),
 DROP COLUMN `num_replicas_in_progress`,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`inode_id`,`block_id`, `target`);
+
+ALTER TABLE `hdfs_inodes` ADD COLUMN `num_aces` int(11) NOT NULL DEFAULT '0';
+
+CREATE TABLE `hdfs_aces` (
+  `inode_id` int(11) NOT NULL,
+  `index` int(11) NOT NULL,
+  `subject` VARCHAR(100) NOT NULL,
+  `type` int NOT NULL DEFAULT '0',
+  `is_default` tinyint NOT NULL DEFAULT '0',
+  `permission` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`inode_id`,`index`),
+  KEY `inode_idx` (`index`)
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs COMMENT='NDB_TABLE=READ_BACKUP=1'
+  /*!50100 PARTITION BY KEY (inode_id) */
