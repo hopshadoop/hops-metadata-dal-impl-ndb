a771229d0ce5ba76b590dea1011cea9753ba4c9b
Jim Dowling <jdowling@sics.se>
2018-02-14 08:39:38 +0100

    flyway failure as table hdfs_storages is redefined in update-schema_2.8.2.2_to_2.8.2.3.sql

--- schema/update-schema_2.8.2.3_to_2.8.2.4.sql
+++ schema/update-schema_2.8.2.3_to_2.8.2.4.sql
@@ -1,8 +1,2 @@
-CREATE TABLE `hdfs_storages` (
-  `host_id` varchar(255) NOT NULL,
-  `storage_id` int(11) NOT NULL,
-  `storage_type` int(11) NOT NULL,
-  PRIMARY KEY (`storage_id`)
-) ENGINE=ndbcluster DEFAULT CHARSET=latin1;
 
 ALTER TABLE `hdfs_inodes` ADD COLUMN `storage_policy` bit(8) NOT NULL DEFAULT '0';
