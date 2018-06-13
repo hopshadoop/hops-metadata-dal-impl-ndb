4c54cd544c637b44d5fdc1d502e5d2ad37ef2eee
Gautier Berthou <gautier.berthou@gmail.com>
2018-06-13 15:07:40 +0200

    [HOPS-485] add 2.8.2.5 to create-tables

--- schema/create-tables.sh
+++ schema/create-tables.sh
@@ -1,4 +1,5 @@
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < schema.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.1_to_2.8.2.2.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.2_to_2.8.2.3.sql &&
-mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.3_to_2.8.2.4.sql
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.3_to_2.8.2.4.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.4_to_2.8.2.5.sql
