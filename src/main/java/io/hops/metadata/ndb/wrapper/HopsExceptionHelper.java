942c08b61ddf55c58f67db2f5e08cf5c0925122a
Gautier Berthou <gautier.berthou@gmail.com>
2018-10-11 16:18:38 +0200

    [HOPS-690] base HopsExceptionHelper.isTransient on classfiscation instead of message.

--- src/main/java/io/hops/metadata/ndb/wrapper/HopsExceptionHelper.java
+++ src/main/java/io/hops/metadata/ndb/wrapper/HopsExceptionHelper.java
@@ -43,23 +43,13 @@ public class HopsExceptionHelper {
     if (e instanceof ClusterJDatastoreException) {
       // http://dev.mysql.com/doc/ndbapi/en/ndb-error-classifications.html
       // The classifications can be found in ndberror.h and ndberror.c in the ndb sources
-      if (e.getMessage().contains("classification 7")) {
-        // Temporary Resource error (TR)
-        return true;
-      } else if (e.getMessage().contains("classification 8")) {
-        // Node Recovery error (NR)
-        return true;
-      } else if (e.getMessage().contains("classification 9")) {
-        // Overload error (OL)
-        return true;
-      } else if (e.getMessage().contains("classification 10")) {
-        // Timeout expired (TO)
-        return true;
-      } else if (e.getMessage().contains("classification 15")) {
-        // Node shutdown (NS)
-        return true;
-      } else if (e.getMessage().contains("classification 18")) {
-        // Internal temporary (IT)
+      int classification = ((ClusterJDatastoreException) e).getClassification();
+      if (classification == ClusterJDatastoreException.Classification.TemporaryResourceError.value || 
+          classification == ClusterJDatastoreException.Classification.NodeRecoveryError.value || 
+          classification == ClusterJDatastoreException.Classification.OverloadError.value || 
+          classification == ClusterJDatastoreException.Classification.TimeoutExpired.value || 
+          classification == ClusterJDatastoreException.Classification.NodeShutdown.value || 
+          classification == ClusterJDatastoreException.Classification.InternalTemporary.value) {
         return true;
       }
     }
