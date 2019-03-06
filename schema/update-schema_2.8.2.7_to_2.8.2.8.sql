3f06663cb58964340ee0515c1cedb99603a7c3f7
Salman Niazi <smkniazi@kth.se>
2019-03-06 17:32:52 +0100

    [HOPS-1151] Reading all replicas for a bucket is a FTS.

--- schema/update-schema_2.8.2.7_to_2.8.2.8.sql
+++ schema/update-schema_2.8.2.7_to_2.8.2.8.sql
@@ -5,3 +5,5 @@ ALTER TABLE `yarn_le_descriptors` ADD COLUMN `location_domain_id` tinyint(4) def
 ALTER TABLE `hdfs_hash_buckets` DROP COLUMN `hash`;
 
 ALTER TABLE `hdfs_hash_buckets` ADD COLUMN `hash` binary(20) NOT NULL DEFAULT '0';
+
+ALTER TABLE hdfs_replicas ADD KEY `storage_and_bucket_idx` (`storage_id`, `bucket_id`);
