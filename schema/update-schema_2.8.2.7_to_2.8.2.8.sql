14c5aa87fb51c94707b019848b23f5bd0cdef88e
Gautier Berthou <gautier.berthou@gmail.com>
2019-03-14 10:15:52 +0100

    [HOPS-1146] partly apply HDFS-7056

--- schema/update-schema_2.8.2.7_to_2.8.2.8.sql
+++ schema/update-schema_2.8.2.7_to_2.8.2.8.sql
@@ -11,3 +11,7 @@ ALTER TABLE hdfs_replicas ADD KEY `storage_and_bucket_idx` (`storage_id`, `bucke
 ALTER TABLE hdfs_inodes DROP KEY `inode_idx`;
 
 ALTER TABLE hdfs_inodes ADD UNIQUE KEY `inode_idx` (`id`);
+
+ALTER TABLE `hdfs_block_infos` ADD COLUMN `truncate_block_num_bytes` bigint(20) NOT NULL DEFAULT '-1';
+
+ALTER TABLE `hdfs_block_infos` ADD COLUMN `truncate_block_generation_stamp` bigint(20) NOT NULL DEFAULT '-1';
