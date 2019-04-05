fd525d197d8e1279c3751f2f6a40a9dd34bf9afd
Gautier Berthou <gautier.berthou@gmail.com>
2019-04-05 10:06:47 +0200

    REVERT HOPS-1113

--- schema/update-schema_2.8.2.7_to_2.8.2.8.sql
+++ schema/update-schema_2.8.2.7_to_2.8.2.8.sql
@@ -8,10 +8,6 @@ ALTER TABLE `hdfs_hash_buckets` ADD COLUMN `hash` binary(20) NOT NULL DEFAULT '0
 
 ALTER TABLE hdfs_replicas ADD KEY `storage_and_bucket_idx` (`storage_id`, `bucket_id`);
 
-ALTER TABLE hdfs_inodes DROP KEY `inode_idx`;
-
-ALTER TABLE hdfs_inodes ADD UNIQUE KEY `inode_idx` (`id`);
-
 ALTER TABLE `hdfs_block_infos` ADD COLUMN `truncate_block_num_bytes` bigint(20) NOT NULL DEFAULT '-1';
 
 ALTER TABLE `hdfs_block_infos` ADD COLUMN `truncate_block_generation_stamp` bigint(20) NOT NULL DEFAULT '-1';
