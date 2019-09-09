23d79ada08119d0600d8ef088ce44b3e1116b023
gautier <gautier.berthou@gmail.com>
2019-09-09 14:07:12 +0200

    fix add update-schema_2.8.2.8_to_3.2.0.0.sql to go directly from 2.8.2.8 to 3.2.0.0

--- schema/update-schema_2.8.2.8_to_3.2.0.0.sql
+++ schema/update-schema_2.8.2.8_to_3.2.0.0.sql
@@ -1,3 +1,6 @@
+ALTER TABLE `hdfs_inodes` CHANGE `num_xattrs` `num_user_xattrs` tinyint(4) NOT NULL DEFAULT '0';
+ALTER TABLE `hdfs_inodes` ADD COLUMN `num_sys_xattrs` tinyint(4) NOT NULL DEFAULT '0';
+ALTER TABLE `hdfs_xattrs` MODIFY COLUMN `value` varbinary(13500) DEFAULT "";
 CREATE TABLE `yarn_conf_mutation` (
   `index` int(11) NOT NULL,
   `mutation` varbinary(13000) NOT NULL,
