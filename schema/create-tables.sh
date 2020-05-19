2abb51b005d04c7abe676a44d72f825ff4e27812
gautier <gautier.berthou@gmail.com>
2020-05-19 17:28:49 +0200

    fix create table for 3.2.0.0 after 2.8.2.10

--- schema/create-tables.sh
+++ schema/create-tables.sh
@@ -7,4 +7,5 @@ mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.5_to_2.8.2.6.sql &
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.6_to_2.8.2.7.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.7_to_2.8.2.8.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.8_to_2.8.2.9.sql &&
-mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.9_to_3.2.0.0.sql
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.9_to_2.8.2.10.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.10_to_3.2.0.0.sql
