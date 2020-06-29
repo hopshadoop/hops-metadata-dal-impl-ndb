222217aaa429ff7617ee45ab4d325bb69ba47368
gautier <gautier.berthou@gmail.com>
2020-06-29 16:01:22 +0200

    [HOPS-1571] ADENDUM variables are put in the database during format, not in the schema definition

--- schema/update-schema_2.8.2.10_to_3.2.0.0.sql
+++ schema/update-schema_2.8.2.10_to_3.2.0.0.sql
@@ -84,5 +84,3 @@ ALTER TABLE hdfs_retry_cache_entry ADD (`epoch` bigint(20) NOT NULL DEFAULT 0);
 ALTER TABLE hdfs_retry_cache_entry DROP PRIMARY KEY;
 
 ALTER TABLE hdfs_retry_cache_entry ADD PRIMARY KEY (`client_id`,`call_id`,`epoch`) PARTITION BY KEY (`epoch`);
-
-INSERT INTO hdfs_variables (id, value) VALUES (39, 0x0000000000000000)
