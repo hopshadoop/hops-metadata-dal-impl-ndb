2edbaf0dd3d17b91b0a23be0db3b2ef7864f54fe
Salman Niazi <salman@logicalclocks.com>
2021-03-09 10:08:40 +0100

    [HOPS-1626] Added missing schema files

--- schema/create-tables.sh
+++ schema/create-tables.sh
@@ -8,4 +8,7 @@ mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.6_to_2.8.2.7.sql &
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.7_to_2.8.2.8.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.8_to_2.8.2.9.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.9_to_2.8.2.10.sql &&
-mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.10_to_3.2.0.0.sql
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_2.8.2.10_to_3.2.0.0.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.0_to_3.2.0.1.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.1_to_3.2.0.2.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.2_to_3.2.0.3.sql
