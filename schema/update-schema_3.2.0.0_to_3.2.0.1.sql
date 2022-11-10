2c2ef78d011970e1add37e5014e48ff480dd388e
Gautier Berthou <gautier@hopsworks.ai>
2022-11-10 15:12:01 +0100

    [CLOUD-81] increase trackingUrl size for yarn applications

--- schema/update-schema_3.2.0.0_to_3.2.0.1.sql
+++ schema/update-schema_3.2.0.0_to_3.2.0.1.sql
@@ -1,3 +1,5 @@
 TRUNCATE TABLE hdfs_le_descriptors;
 
 TRUNCATE TABLE yarn_le_descriptors;
+
+ALTER TABLE yarn_applicationattemptstate MODIFY COLUMN applicationattempttrakingurl VARCHAR(300) DEFAULT NULL
