f6c7a89562157086d476cdcb7569e1d30a3a3152
Salman Niazi <salman@logicalclocks.com>
2023-04-19 15:25:30 +0200

    HOPSFS-99: Added schema update files

--- schema/create-tables.sh
+++ schema/create-tables.sh
@@ -16,4 +16,5 @@ mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.3_to_3.2.0.4.sql &
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.4_to_3.2.0.5.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.6_to_3.2.0.7.sql &&
 mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.7_to_3.2.0.8.sql &&
-mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.8_to_3.2.0.9.sql
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.8_to_3.2.0.9.sql &&
+mysql --host=$1 --port=$2 -u $3 -p$4 $5 < update-schema_3.2.0.9_to_3.2.0.10.sql
